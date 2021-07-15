5ee53a3dc44e5fc221d32c243270d53c490b5b7f
Emmanuel Campuzano <emmanuel@Nitro5.localdomain>
2021-07-14 21:10:06 -0600

    Video & env for the DB

--- backend/src/db.py
+++ backend/src/db.py
@@ -1,9 +1,19 @@
+from decouple import config
 import pymysql
 
 def getConection():
+
+    DATABASE_URL = config('DATABASE_URL')
+    USER = config('USER')
+    PASSWORD = config('PASSWORD')
+
+    print(DATABASE_URL)
+    print(USER)
+    print(PASSWORD)
+
     return pymysql.connect(
-        host = 'localhost',
-        user = 'emmanuel',
-        password = '895621',
+        host = DATABASE_URL,
+        user = USER,
+        password = PASSWORD,
         db = 'home'
     )
